73bf8e998e17abc70b9624683910e8c22f398a38
Rohit singh <raj0eshsing0909@gmail.com>
2024-09-29 03:22:54 +0530

    added the pdf download functionality and ui enhancement

--- components/TextEditor.tsx
+++ components/TextEditor.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-
 import { ClientSideSuspense } from "@liveblocks/react";
 
 import { LiveblocksYjsProvider } from "@liveblocks/yjs";
@@ -49,40 +48,29 @@ import styles from "./TextEditor.module.css";
 
 import { Avatars } from "@/components/Avatars";
 
+import {jsPDF} from "jspdf"
+
 
 export function TextEditor() {
-
   return (
-
     <ClientSideSuspense fallback={<DocumentSpinner />}>
-
       <Editor />
-
     </ClientSideSuspense>
-
   );
-
 }
-
 
 // Collaborative text editor with simple rich text and live cursors
 
 export function Editor() {
-
   const room = useRoom();
 
   const [doc, setDoc] = useState<Y.Doc>();
 
   const [provider, setProvider] = useState<any>();
 
-  
-
-
-
   // Set up Liveblocks Yjs provider
 
   useEffect(() => {
-
     const yDoc = new Y.Doc();
 
     const yProvider = new LiveblocksYjsProvider(room, yDoc);
@@ -91,111 +79,72 @@ export function Editor() {
 
     setProvider(yProvider);
 
-
     return () => {
-
       yDoc?.destroy();
 
       yProvider?.destroy();
-
     };
-
   }, [room]);
 
-
   if (!doc || !provider) {
-
     return null;
-
   }
 
-
   return <TiptapEditor doc={doc} provider={provider} />;
-
 }
 
-
 type EditorProps = {
-
   doc: Y.Doc;
 
   provider: any;
-
 };
 
-
 function TiptapEditor({ doc, provider }: EditorProps) {
-
   // Get user info from Liveblocks authentication endpoint
 
   const { name, color, picture } = useSelf((me) => me.info);
 
-
-
   // Set up editor with plugins, and place user info into Yjs awareness and cursors
 
   const editor = useEditor({
-
     editorProps: {
-
       attributes: {
-
         // Add styles to editor element
 
         class: styles.editor,
-
       },
-
     },
 
     extensions: [
-
       StarterKit.configure({
-
         blockquote: {
-
           HTMLAttributes: {
-
             class: "tiptap-blockquote",
-
           },
-
         },
 
         code: {
-
           HTMLAttributes: {
-
             class: "tiptap-code",
-
           },
-
         },
 
         codeBlock: {
-
           languageClassPrefix: "language-",
 
           HTMLAttributes: {
-
             class: "tiptap-code-block",
 
             spellcheck: false,
-
           },
-
         },
 
         heading: {
-
           levels: [1, 2, 3],
 
           HTMLAttributes: {
-
             class: "tiptap-heading",
-
           },
-
         },
 
         // The Collaboration extension comes with its own history handling
@@ -203,190 +152,189 @@ function TiptapEditor({ doc, provider }: EditorProps) {
         history: false,
 
         horizontalRule: {
-
           HTMLAttributes: {
-
             class: "tiptap-hr",
-
           },
-
         },
 
         listItem: {
-
           HTMLAttributes: {
-
             class: "tiptap-list-item",
-
           },
-
         },
 
         orderedList: {
-
           HTMLAttributes: {
-
             class: "tiptap-ordered-list",
-
           },
-
         },
 
         paragraph: {
-
           HTMLAttributes: {
-
             class: "tiptap-paragraph",
-
           },
-
         },
-
       }),
 
       Highlight.configure({
-
         HTMLAttributes: {
-
           class: "tiptap-highlight",
-
         },
-
       }),
 
       Image.configure({
-
         HTMLAttributes: {
-
           class: "tiptap-image",
-
         },
-
       }),
 
       Link.configure({
-
         HTMLAttributes: {
-
           class: "tiptap-link",
-
         },
-
       }),
 
       Placeholder.configure({
-
         placeholder: "Start writing…",
 
         emptyEditorClass: "tiptap-empty",
-
       }),
 
       CustomTaskItem,
 
       TaskList.configure({
-
         HTMLAttributes: {
-
           class: "tiptap-task-list",
-
         },
-
       }),
 
       TextAlign.configure({
-
         types: ["heading", "paragraph"],
-
       }),
 
       Typography,
 
       Youtube.configure({
-
         modestBranding: true,
 
         HTMLAttributes: {
-
           class: "tiptap-youtube",
-
         },
-
       }),
 
       // Register the document with Tiptap
 
       Collaboration.configure({
-
         document: doc,
-
       }),
 
       // Attach provider and user info
 
       CollaborationCursor.configure({
-
         provider: provider,
 
         user: {
-
           name,
 
           color,
 
           picture,
-
         },
-
       }),
-
     ],
-
   });
 
-  const handleLog = () => {
-    if(editor) {
-      const content = editor.getJSON();
-      console.log("this is the content", content);
-    }
-  }
+  // we are creating the new instance of yjs docs  so the written content will come here in the form of json
 
+  // this is the function that takes out written text in an editor 
+
+  function ExtractFiles(nodes: any){
+    let textContent = "";
+    console.log("this is a nodes", nodes);
+
+    nodes.forEach((temp: any) => {
+    if (temp.type === "paragraph") {
+      temp?.content?.forEach((item: any) => {
+        if (item.type === "text") {
+          textContent += item.text + " ";
+        }
+      });
+    } else if (temp.type === "orderedList") {
+      temp?.content?.forEach((OrderedItem: any) => {
+        if (OrderedItem.type === "listItem") {
+          OrderedItem.content.forEach((items: any) => {
+            if (items.type === "paragraph") {
+              items?.content.forEach((textItem: any) => {
+                if (textItem?.type === "text") {
+                  textContent += textItem.text + " ";
+                }
+              });
+            }
+          });
+        }
+      });
+    }
+  })
+
+    return textContent;
+  };
+
+
+  const handleDownloadPdf = () => {
+    if (editor) {
+      const data = editor.getJSON();
+      let res = data.content;
+      const extractedText = ExtractFiles(res);
+
+      const pdf = new jsPDF();
+      pdf.text(extractedText, 10, 10);
+      pdf.save("text-file.pdf");
+    }
+  };
+
+  // Object structure of data in aur app
+  // const data = [
+  //   {
+  //     type: "paragraph",
+  //     content: [{ type: "text", text: "Hi I am Rohit Singh Khetwal" }]
+  //   },
+  //   {
+  //     type: "orderedList",
+  //     content: [
+  //       { type: "listItem", content: [{ type: "paragraph", content: [{ type: "text", text: "learn about nodejs authentication" }] }] },
+  //       { type: "listItem", content: [{ type: "paragraph", content: [{ type: "text", text: "study about 3 pointer in data structure " }] }] },
+  //       { type: "listItem", content: [{ type: "paragraph", content: [{ type: "text", text: "maximun sub tree" }] }] },
+  //       { type: "listItem", content: [{ type: "paragraph", content: [{ type: "text", text: "I know nothing " }] }] },
+  //       // More items...
+  //     ]
+  //   }
+  // ];
 
   return (
-
     <div className={styles.container}>
-
       <div className={styles.editorHeader}>
-
         {editor && <Toolbar editor={editor} />}
 
         <Avatars />
-
       </div>
 
       <div className={styles.editorPanel}>
-
         {editor && <SelectionMenu editor={editor} />}
-
         <EditorContent editor={editor} className={styles.editorContainer} />
-
       </div>
-      <button onClick={handleLog}>
-        Log Content
+
+      <button onClick={handleDownloadPdf} className="bg-violet-400 px-3 py-1 rounded mb-2">
+        Download as Pdf
 
       </button>
-
+      
     </div>
-
   );
-
 }
-
 
 // Prevents a matchesNode error on hot reloading
 
 EditorView.prototype.updateState = function updateState(state) {
-
   // @ts-ignore
 
   if (!this.docView) return;
@@ -394,5 +342,4 @@ EditorView.prototype.updateState = function updateState(state) {
   // @ts-ignore
 
   this.updateStateInner(state, this.state.plugins != state.plugins);
-
 };
